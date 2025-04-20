1d11d26ea853a600d28b5c2732c5483bd6fd1b38
MakhloufiAdnan <makhloufiadnan@gmail.com>
2025-04-20 20:10:06 +0200

    Debug init.sh

--- frontend/init.sh
+++ frontend/init.sh
@@ -7,10 +7,8 @@ export API_BASE_URL="${API_BASE_URL:-localhost:8080}"
 echo "PORT is: $PORT"
 echo "API_BASE_URL is: $API_BASE_URL"
 
-# Substitution des variables dans la conf nginx, explicitly listing variables
-envsubst -v PORT -v API_BASE_URL \
-  < /etc/nginx/nginx.conf.template \
-  > /etc/nginx/nginx.conf
+# Substitution des variables dans la conf nginx (back to standard syntax)
+envsubst < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # Debug : afficher le contenu généré
 echo "======= nginx.conf généré ======="

5dd29c97c7008628fd682e5b570279ba5fedd506
MakhloufiAdnan <makhloufiadnan@gmail.com>
2025-04-20 22:02:45 +0200

    Debug nginx.conf&init

--- frontend/init.sh
+++ frontend/init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Explicitly set default values (Heroku should override PORT)
+# Valeurs par défaut si non définies par Heroku
 export PORT="${PORT:-80}"
 export API_BASE_URL="${API_BASE_URL:-localhost:8080}"
 
@@ -8,7 +8,7 @@ echo "PORT is: $PORT"
 echo "API_BASE_URL is: $API_BASE_URL"
 
 # Substitution des variables dans la conf nginx
-envsubst < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${PORT} ${API_BASE_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # Debug : afficher le contenu généré
 echo "======= nginx.conf généré ======="

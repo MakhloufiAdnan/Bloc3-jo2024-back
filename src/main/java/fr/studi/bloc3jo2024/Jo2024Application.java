57f449c01d08ca601f5a87719b9182d9a772e04f
MakhloufiAdnan <makhloufiadnan@gmail.com>
2025-06-12 12:14:42 +0200

    log appli

--- src/main/java/fr/studi/bloc3jo2024/Jo2024Application.java
+++ src/main/java/fr/studi/bloc3jo2024/Jo2024Application.java
@@ -1,14 +1,19 @@
 package fr.studi.bloc3jo2024;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Jo2024Application {
 
+	private static final Logger log = LoggerFactory.getLogger(Jo2024Application.class);
     public static void main(String[] args) {
 		SpringApplication.run(Jo2024Application.class, args);
 	}
 
-
+	static {
+		log.info("=== Lancement application Jo2024 ===");
+	}
 }
